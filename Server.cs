1102a3036271bbcb98de5f1b0ebe899d4d79a0ed
denielleoliva <denielle.oliva@gmail.com>
2022-03-28 13:50:29 -0700

    server side of send file works

--- Server.cs
+++ Server.cs
@@ -4,7 +4,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.IO;
 using System.Text;
-using System.Security.Cryptography.MD5;
+using System.Security.Cryptography;
 
 namespace Server{
 
@@ -23,6 +23,39 @@ namespace Server{
                 TcpClient client = listener.AcceptTcpClient();
 
                 Console.Write("Connection Established...");
+
+                StreamReader reader = new StreamReader(client.GetStream());
+
+                string fileSize = reader.ReadLine();
+
+                string fileName = reader.ReadLine();
+
+                int len = Convert.ToInt32(fileSize);
+                byte[] buffer = new byte[len];
+                int received = 0;
+                int read = 0;
+                int size = 1024;
+                int remaining = 0;
+
+                while(received<len){
+                    remaining = len - received;
+                    if(remaining<size){
+                        size = remaining;
+                    }
+
+
+                    read = client.GetStream().Read(buffer, received, size);
+                    received+=read;
+                }
+
+                using(FileStream fileStream = new FileStream(Path.GetFileName(fileName), FileMode.Create)){
+                    fileStream.Write(buffer, 0, buffer.Length);
+                    fileStream.Flush();
+                    fileStream.Close();
+                }
+
+
+                Console.WriteLine(Environment.CurrentDirectory);
 
             }
 
