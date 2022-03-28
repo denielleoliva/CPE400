a3ea6a7be1c1f6feafbb2bd5d39045188e828a0e
denielleoliva <denielle.oliva@gmail.com>
2022-03-28 10:38:33 -0700

    working

--- FileTransferClient.cs
+++ FileTransferClient.cs
@@ -1,8 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
 
-public class FileTransferClient
-{
-	public FileTransferClient()
-	{
+namespace FileTransferClient{
+
+	class Program{
+
+		static void Main(string[] args){
+
+			Console.WriteLine(args[0]);
+			Console.WriteLine(args[1]);
+
+			TcpClient client = new TcpClient("127.0.0.1", 1234);
+			Console.WriteLine("Connection established....");
+
+			
+
+
+		}
+
 	}
+
 }
