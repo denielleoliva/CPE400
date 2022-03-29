04d5bbf19b29ae3961a5d913e1ba4f59342bd827
Jasonliangnv <64287679+Jasonliangnv@users.noreply.github.com>
2022-03-28 20:36:19 -0700

    Create ChecksumCheck.cs

---
+++ ChecksumCheck.cs
@@ -0,0 +1,16 @@
+using System;
+using System.IO;
+using System.Text;
+
+class Checksum
+{
+  public static double assignChecksum()
+  {
+    return 0;
+  }
+  
+  public static double checkChecksum()
+  {
+    return 0;
+  }
+}
